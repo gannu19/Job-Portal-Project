30fa465e52bd3adc7c2eb5067f4c0274b791d5b8
gannu19 <bhukyaganapathi00@gmail.com>
2026-04-04 16:55:44 +0530

    Added the clerk authorization for the project and designed the home navigation bar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
 import {Route, Routes} from 'react-router-dom';
+import Home from './Pages/Home';
+import Applications from './Pages/Applicatons';
+import ApplyJob from './Pages/ApplyJob';
 
 const App =()=>{
   return (
-    <div>App</div>
+    <div>
+      <Routes>
+        <Route path = '/' element={<Home />}></Route>
+        <Route path = '/apply-job/:id' element={<ApplyJob />}></Route>
+        <Route path = '/applications' element={<Applications />}></Route>
+      </Routes>
+    </div>
   )
 }
 export default App;
